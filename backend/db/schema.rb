05fdd4b3daee3881f97be5013b1698ae17b36258
Ashil <ashil.farahmand@gmail.com>
2025-11-05 16:51:08 -0500

    Added rewards table

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_05_152503) do
+ActiveRecord::Schema[8.1].define(version: 2025_11_05_153228) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -29,6 +29,16 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_05_152503) do
     t.index ["funding_amount"], name: "index_projects_on_funding_amount"
     t.index ["funding_end_date"], name: "index_projects_on_funding_end_date"
     t.index ["user_id"], name: "index_projects_on_user_id"
+  end
+
+  create_table "rewards", force: :cascade do |t|
+    t.integer "amount", null: false
+    t.datetime "created_at", null: false
+    t.string "description"
+    t.bigint "project_id"
+    t.string "title", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_rewards_on_project_id"
   end
 
   create_table "users", force: :cascade do |t|
