94f8d75d93d986e489e8a1137b08485e84fed680
Ashil <ashil.farahmand@gmail.com>
2025-11-18 12:06:07 -0500

    Updated Rails cors config to allow requests from FE app

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,8 +1,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:5173', '0.0.0.0:5173'
+    origins 'http://localhost:5173', 'localhost:5173', '0.0.0.0:5173', 'http://frontend:5173'
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
   end
 end
