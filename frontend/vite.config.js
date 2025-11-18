2a59e918a581d489a6c9590d77c8b95865ec666a
Ashil <ashil.farahmand@gmail.com>
2025-11-18 12:05:41 -0500

    Added env var for FE Vite app

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  return {
+    plugins: [react()],
+    // Define global constants
+    define: {
+      'import.meta.env.KICKS_API_BASE_URL': JSON.stringify(
+        process.env.KICKS_API_BASE_URL || env.KICKS_API_BASE_URL
+      )
+    },
+  }
 })
