743ec6bf318ef09e2046bc074f41c03ca1f6d749
Ashil <ashil.farahmand@gmail.com>
2025-11-29 09:04:59 -0500

    updated Auth and Protected routes

--- frontend/src/utils/route_util.jsx
+++ frontend/src/utils/route_util.jsx
@@ -1,32 +1,17 @@
-import { Route } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-import { Redirect } from '../components/redirect'
-import { withRouter } from '../components/withRouter';
-
-const Auth = ({component: Component, path, signedIn, exact}) => (
-  <Route path={path} exact={exact} render={(props) => (
-    !signedIn ? (
-      <Component {...props} />
-    ) : (
-      <Redirect to="/" />
-    )
-  )}/>
-);
-
-const Protected = ({ component: Component, path, signedIn, exact }) => (
-  <Route path={path} exact={exact} render={(props) => (
-     signedIn ? (
-      <Component {...props} />
-    ) : (
-      <Redirect to="/signin" />
-    )
-  )} />
-);
-
-const mapStateToProps = state => {
-  return {signedIn: Boolean(state.session.currentUser)};
+const Auth = ({ children, signedIn }) => {
+  return !signedIn ? children : <Navigate to="/" replace />;
 };
 
-export const AuthRoute = withRouter(connect(mapStateToProps, null)(Auth));
-export const ProtectedRoute = withRouter(connect(mapStateToProps, null)(Protected));
+const Protected = ({ children, signedIn }) => {
+  return signedIn ? children : <Navigate to="/signin" replace />;
+};
+
+const mapStateToProps = state => {
+  return { signedIn: Boolean(state.session.currentUser) };
+};
+
+export const AuthRoute = connect(mapStateToProps, null)(Auth);
+export const ProtectedRoute = connect(mapStateToProps, null)(Protected);
