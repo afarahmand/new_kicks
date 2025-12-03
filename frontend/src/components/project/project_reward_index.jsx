9601aa4c89cb3839cad58a3e1f2a2c30a5ec64ef
Ashil <ashil.farahmand@gmail.com>
2025-12-03 09:05:13 -0500

    Fixed bug when rendering project rewards

--- frontend/src/components/project/project_reward_index.jsx
+++ frontend/src/components/project/project_reward_index.jsx
@@ -6,17 +6,17 @@ import selectAlreadyBacked from '../../selectors/already_backed';
 import selectProjectRewards from '../../selectors/project_rewards';
 
 const ProjectRewardIndex = ({ project }) => {
-    const currentUser = useSelector((state) => (state.session.currentUser));
+    const currentUser = useSelector(state => state.session.currentUser);
     const projectRewards = useSelector(selectProjectRewards(project.id));
     const projectRewardIds = projectRewards.map(reward => reward.id);
     const alreadyBacked = useSelector(selectAlreadyBacked(projectRewardIds, currentUser));
 
     const dispatch = useDispatch();
-    const dispatchCreateBacking = (backing) => dispatch(createBacking(backing));
+    const dispatchCreateBacking = backing => dispatch(createBacking(backing));
 
     if (currentUser) {
         if (currentUser.id === project.user_id) {
-            return ( <div className="col-3 rewards"></div> );
+            return ( <div className="col-3 rewards"></div> )
         } else if (alreadyBacked) {
             return (
                 <div className="col-3 rewards">
@@ -24,22 +24,22 @@ const ProjectRewardIndex = ({ project }) => {
                 </div>
             )
         }
-    } else {
-        return (
-            <div className="col-3 rewards">
-                <h3>Support</h3>
-                    {
-                        projectRewards.map((reward, idx) => (
-                            <RewardIndexDisplayItem
-                                key={idx}
-                                reward={reward}
-                                createBacking={dispatchCreateBacking}
-                            />
-                        ))
-                    }
-            </div>
-        )
     }
+
+    return (
+        <div className="col-3 rewards">
+            <h3>Support</h3>
+                {
+                    projectRewards.map((reward, idx) => (
+                        <RewardIndexDisplayItem
+                            key={idx}
+                            reward={reward}
+                            createBacking={dispatchCreateBacking}
+                        />
+                    ))
+                }
+        </div>
+    )
 }
 
 export default ProjectRewardIndex;
