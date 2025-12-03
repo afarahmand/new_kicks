2f6769c24cabb21a413307610911feebd350ff17
Ashil <ashil.farahmand@gmail.com>
2025-12-02 19:04:31 -0500

    Added ErrorIndex to reward form

--- frontend/src/components/reward/reward_form.jsx
+++ frontend/src/components/reward/reward_form.jsx
@@ -11,11 +11,12 @@ import { selectProjectRewards } from '../../selectors/project_rewards';
 
 import RewardFormIndexItem from './reward_form_index_item';
 import NewRewardIndex from './new_reward_index';
+import ErrorIndex from '../shared/error_index';
 
 const RewardForm = () => {
     const { projectId } = useParams();
     const sortedProjectRewards = useSelector(selectProjectRewards(parseInt(projectId)));
-    // const errors = useSelector(state => state.errors.rewards);
+    const errors = useSelector(state => state.errors.rewards);
 
     const dispatch = useDispatch();
     const dispatchFetchProject = () => dispatch(fetchProject(projectId));
@@ -35,6 +36,7 @@ const RewardForm = () => {
                 offering rewards like a copy of what you're making, a special
                 experience, or a behind-the-scenes look into your process.
             </section>
+            <ErrorIndex errors={errors} />
             <section className="reward-form-container">
                 {
                     sortedProjectRewards.map(reward => (
