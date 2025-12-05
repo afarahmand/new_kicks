31cc4c87a2220f5d1a29780b0232d242e9bb4237
Ashil <ashil.farahmand@gmail.com>
2025-12-04 19:43:46 -0500

    Simplified reward sort helper method

--- frontend/src/utils/sort_util.js
+++ frontend/src/utils/sort_util.js
@@ -1,11 +1,3 @@
 export const sortByAscendingAmount = rewardsArray => (
-    rewardsArray.sort(function(reward1, reward2) {
-        if (reward1.amount < reward2.amount) {
-            return -1;
-        } else if (reward1.amount > reward2.amount) {
-            return 1;
-        } else {
-            return 0;
-        }
-    })
+    rewardsArray.sort((a, b) => a.amount - b.amount)
 )
