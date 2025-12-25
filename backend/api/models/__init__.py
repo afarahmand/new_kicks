0714bd1d5b93fba10fc9608c121aed8ac51f706f
Ashil <ashil.farahmand@gmail.com>
2025-12-24 19:48:01 -0500

    Added user model

---
+++ backend/api/models/__init__.py
@@ -0,0 +1,4 @@
+from .user import User
+
+# Optional: define __all__ for explicit exports
+__all__ = ['User']
---
+++ backend/api/models/user.py
@@ -0,0 +1,31 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.db import models
+from ..mixins.timestamps import TimestampMixin
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+class User(AbstractBaseUser, TimestampMixin):
+    REQUIRED_FIELDS = ['name']
+    USERNAME_FIELD = 'email'
+
+    email = models.EmailField(null=False, unique=True)
+    image_url = models.CharField(null=False, default="https://i.imgur.com/rfxjQeS.png")
+    name = models.CharField(null=False)
+
+    objects = UserManager()
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['email']),
+        ]
+
+    def __str__(self):
+        return f"{self.name} - {self.email}"
