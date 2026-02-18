c5572d8d1ca5c5318b5714414076af49d260c378
Ashil <17262181+afarahmand@users.noreply.github.com>
2026-02-18 16:43:23 -0500

    Changed url and title to quikstarter

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   
   return {
-    base: '/new_kicks_frontend/',
+    base: '/quikstarter/',
     define: {
       'import.meta.env.BASE_URL_KICKS_API': JSON.stringify(
         process.env.BASE_URL_KICKS_API || env.BASE_URL_KICKS_API
